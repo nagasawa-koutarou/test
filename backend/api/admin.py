ceb84c850abef4ddaae4971d07a6cfd0094746dc
nagasawa-koutarou <curarakoutarou@gmail.com>
2025-05-05 06:30:23 +0900

    Add Memo model and register it in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Memo
+
+@admin.register(Memo)
+class MemoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'user')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at',)
 
 # Register your models here.
